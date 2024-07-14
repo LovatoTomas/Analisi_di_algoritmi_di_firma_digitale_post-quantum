bd68a3fde00bef1a8cc8e58dd4732dd17e888b5b
Tomas Lovato <tomas.lovato.personal@gmail.com>
2024-07-14 22:22:15 +0200

    Python Analytics REV2
    Aggiornato il codice Python per:
    - gestire le nuove metriche di performance per CRYSTALS Dilithium e FALCON.
    - poter essere eseguito in ambienti non grafici (es: Linux / WSL),

--- 20240713_performancegraphs.py
+++ 20240713_performancegraphs.py
@@ -4,30 +4,42 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+# Variabile globale per decidere se mostrare o salvare i grafici
+SHOW_PLOTS = False
 
 # Funzione per leggere i dati dal file
 def read_data(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
 
-    #for riga in lines:
-        # Fai qualcosa con la riga
-        #print(riga.strip())
-
     data = []
     for line in lines:
         line = line.strip()
         if line:
             fields = line.split('|')
-            data.append([int(fields[1]), int(fields[2]), int(fields[3]), int(fields[4]), int(fields[5]), float(fields[6]), int(fields[7]), float(fields[8]), int(fields[9]), float(fields[10]), int(fields[11]), float(fields[12]), int(fields[13])])
+            data.append([int(fields[1]), int(fields[2]), int(fields[3]), int(fields[4]), int(fields[5]), float(fields[6]), float(fields[7]), float(fields[8]), int(fields[9])])
 
-    columns = ['msg_len', 'signed_msg_len', 'pub_key_size', 'priv_key_size', 'signature_size', 'keygen_time', 'keygen_cycles', 'sign_time', 'sign_cycles', 'verify_time_correct', 'verify_cycles_correct', 'verify_time_corrupt', 'verify_cycles_corrupt']
+    columns = ['msg_len', 'signed_msg_len', 'pub_key_size', 'priv_key_size', 'signature_size', 'keygen_time', 'sign_time', 'verify_time_correct', 'hash_len']
 
     df = pd.DataFrame(data, columns=columns)
     return df
 
+# Funzione per salvare o mostrare il grafico
+def save_or_show_plot(filename):
+    if SHOW_PLOTS:
+        plt.show()
+    else:
+        directory = os.path.dirname(filename)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        plt.savefig(filename)
+    plt.clf()
+
 # Leggere il file caricato
-file_path = './CRYSTALS-dilithium/ref/20240713_output_dilithium2.txt'
+file_path = './output/dilithium2_avx2'
 df = read_data(file_path)
 
 # Grafico della dimensione delle chiavi al variare della lunghezza del messaggio
@@ -41,7 +53,7 @@ plt.legend()
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/key_sizes_vs_message_length.png')
 
 # Grafico della dimensione del messaggio originale e firmato al variare della lunghezza del messaggio
 plt.figure(figsize=(10, 6))
@@ -52,16 +64,15 @@ plt.ylabel('Length (bytes)')
 plt.title('Original and Signed Message Length vs Message Length')
 plt.legend()
 plt.grid(True)
-plt.xscale('log')  # Usare scala logaritmica per gestire un ampio intervallo di valori
+plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/original_and_signed_message_length.png')
 
 # Grafico dei tempi di keygen, firma e verifica al variare della lunghezza del messaggio
 plt.figure(figsize=(10, 6))
 plt.plot(df['msg_len'], df['keygen_time'], label='Keygen Time')
 plt.plot(df['msg_len'], df['sign_time'], label='Sign Time')
 plt.plot(df['msg_len'], df['verify_time_correct'], label='Verify Time (Correct)')
-plt.plot(df['msg_len'], df['verify_time_corrupt'], label='Verify Time (Corrupt)')
 plt.xlabel('Message Length (bytes)')
 plt.ylabel('Time (seconds)')
 plt.title('Keygen, Sign, and Verify Times vs Message Length')
@@ -69,8 +80,9 @@ plt.legend()
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/keygen_sign_verify_times.png')
 
+# Grafico del tempo di keygen al variare della lunghezza del messaggio
 plt.figure(figsize=(10, 6))
 plt.plot(df['msg_len'], df['keygen_time'], label='Keygen Time')
 plt.xlabel('Message Length (bytes)')
@@ -80,8 +92,9 @@ plt.legend()
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/keygen_time_vs_message_length.png')
 
+# Grafico del tempo di firma al variare della lunghezza del messaggio
 plt.figure(figsize=(10, 6))
 plt.plot(df['msg_len'], df['sign_time'], label='Sign Time')
 plt.xlabel('Message Length (bytes)')
@@ -91,11 +104,11 @@ plt.legend()
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/sign_time_vs_message_length.png')
 
+# Grafico del tempo di verifica al variare della lunghezza del messaggio
 plt.figure(figsize=(10, 6))
 plt.plot(df['msg_len'], df['verify_time_correct'], label='Verify Time (Correct)')
-plt.plot(df['msg_len'], df['verify_time_corrupt'], label='Verify Time (Corrupt)')
 plt.xlabel('Message Length (bytes)')
 plt.ylabel('Time (seconds)')
 plt.title('Verify Time vs Message Length')
@@ -103,21 +116,18 @@ plt.legend()
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/verify_time_vs_message_length.png')
 
 # Funzione per leggere e unire i dati da più file
 def read_multiple_files(filenames):
     dataframes = [read_data(file) for file in filenames]
     for df, file in zip(dataframes, filenames):
-        df['algorithm'] = file.split('_')[2].split('.')[0]  # estrai il nome dell'algoritmo dal nome del file
+        df['algorithm'] = file.split('/')[2]  # estrai il nome dell'algoritmo dal nome del file
     return pd.concat(dataframes, ignore_index=True)
 
 # Leggere i file di esempio (sostituire con i nomi reali dei file)
-file_paths = ['./CRYSTALS-dilithium/ref/20240713_output_dilithium2.txt', './CRYSTALS-dilithium/ref/20240713_output_dilithium3.txt', './CRYSTALS-dilithium/ref/20240713_output_dilithium5.txt']
+file_paths = ['./output/dilithium2_avx2', './output/dilithium2_sha256_avx2', './output/falcon2_avx2', './output/falcon2_avx2_sha256']
 df_all = read_multiple_files(file_paths)
-
-# Grafici di confronto tra algoritmi
-import seaborn as sns
 
 # 2.1 Confronto della dimensione delle chiavi tra algoritmi
 plt.figure(figsize=(10, 6))
@@ -130,7 +140,7 @@ plt.legend()
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/comparison_key_sizes_vs_message_length.png')
 
 # 2.2 Confronto della dimensione del messaggio firmato tra algoritmi, mostrando anche il messaggio originale
 plt.figure(figsize=(10, 6))
@@ -141,16 +151,15 @@ plt.ylabel('Length (bytes)')
 plt.title('Original and Signed Message Length vs Message Length (Comparison)')
 plt.legend()
 plt.grid(True)
-plt.xscale('log')  # Usare scala logaritmica per gestire un ampio intervallo di valori
+plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/comparison_original_and_signed_message_length.png')
 
 # 2.3 Confronto dei tempi di keygen, firma e verifica tra algoritmi
 plt.figure(figsize=(10, 6))
 sns.lineplot(x='msg_len', y='keygen_time', hue='algorithm', data=df_all, marker='o')
 sns.lineplot(x='msg_len', y='sign_time', hue='algorithm', data=df_all, marker='o')
 sns.lineplot(x='msg_len', y='verify_time_correct', hue='algorithm', data=df_all, marker='o')
-sns.lineplot(x='msg_len', y='verify_time_corrupt', hue='algorithm', data=df_all, marker='o')
 plt.xlabel('Message Length (bytes)')
 plt.ylabel('Time (seconds)')
 plt.title('Keygen, Sign, and Verify Times vs Message Length (Comparison)')
@@ -158,8 +167,9 @@ plt.legend()
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/comparison_keygen_sign_verify_times.png')
 
+# Confronto del tempo di keygen tra algoritmi
 plt.figure(figsize=(10, 6))
 sns.lineplot(x='msg_len', y='keygen_time', hue='algorithm', data=df_all, marker='o')
 plt.xlabel('Message Length (bytes)')
@@ -169,8 +179,9 @@ plt.legend(title='Algorithm')
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/comparison_keygen_time_vs_message_length.png')
 
+# Confronto del tempo di firma tra algoritmi
 plt.figure(figsize=(10, 6))
 sns.lineplot(x='msg_len', y='sign_time', hue='algorithm', data=df_all, marker='o')
 plt.xlabel('Message Length (bytes)')
@@ -180,15 +191,16 @@ plt.legend(title='Algorithm')
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/comparison_sign_time_vs_message_length.png')
 
+# Confronto del tempo di verifica tra algoritmi
 plt.figure(figsize=(10, 6))
-sns.lineplot(x='msg_len', y='sign_time', hue='algorithm', data=df_all, marker='o')
+sns.lineplot(x='msg_len', y='verify_time_correct', hue='algorithm', data=df_all, marker='o')
 plt.xlabel('Message Length (bytes)')
-plt.ylabel('Sign Time (seconds)')
-plt.title('Sign Time vs Message Length (Comparison)')
+plt.ylabel('Verify Time (seconds)')
+plt.title('Verify Time vs Message Length (Comparison)')
 plt.legend(title='Algorithm')
 plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+save_or_show_plot('./plot/comparison_verify_time_vs_message_length.png')
